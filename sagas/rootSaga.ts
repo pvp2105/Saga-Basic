68eb76d745d19ae037abb243a4df3635ce04dcee
pvp2105 <phuanh2105@gmail.com>
2023-07-10 16:51:30 +0700

    axios_interceptors

--- sagas/rootSaga.ts
+++
@@ -1,9 +0,0 @@
-import { all } from 'redux-saga/effects';
-import { watchFetchDataNews } from './newsSaga';
-
-export default function* rootSaga() {
-    yield all([
-        watchFetchDataNews(),
-        // Thêm các saga khác nếu cần thiết
-    ]);
-}
